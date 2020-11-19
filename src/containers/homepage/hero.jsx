4c6a3701e6851863ec8de47fe6f73553de1dd807
Haylee McLemore <hmmclemore@gmail.com>
2020-11-19 11:01:58 -0500

    creating hero component

---
+++ src/containers/homepage/hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import BackgroundImg from "../../assets/pictures/bg.jpg";
+
+const HeroContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 0;
+  background-image: url(${BackgroundImg});
+`;
+
+export function Hero(props) {
+  return <Hero>Test</Hero>;
+}
---
+++ src/containers/homepage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PageContainer = stuled.div`
+    width:100%;
+    height:100%;
+    `
+
+export function Homepage(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
